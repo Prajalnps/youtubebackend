072c4d66e042e67dea5eb6195df67c35549d81b2
Prajaldev25 <128599282+Prajaldev25@users.noreply.github.com>
2024-01-04 16:01:09 +0545

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,73 @@
+// require('dotenv').config({ path: './env' });
+import dotenv from "dotenv"
+import connectDB from "./db/connect.js";
+
+dotenv.config({
+    path: './env',
+});
+/*
+-r dotenv/config --experimental-json-modules, add this to package.json dev property
+*/
+
+/*
+Second approach, better one, another file from db folder, connect db in that file,
+export that file and import in index.js file
+*/
+
+connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//First approach to connect the db within the index.js
+/*
+const connectDB = () => {
+ 
+}
+connectDB();
+ 
+    - we can make this code better by using IIFE
+    - An IIFE (Immediately Invoked Function Expression) is a function 
+    that runs the moment it is invoked or called in the JavaScript event loop
+*/
+
+/*
+import express from "express";
+
+const app = express();
+
+//good approach: connecting db in a module/in IIFE/Not polluting anything outside
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) //DB connect, we also have to add / and db name, use constants.js that we created
+        //when db is connected, what you see next is listeners
+        app.on("error", (error) => {
+            console.log("ERROR: ", error)
+            throw err
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err //you can throw error or process exit
+    }
+})() //professional approach to add a semicolon at the beginning
+*/
+
